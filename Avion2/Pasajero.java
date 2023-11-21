90044dc72a80cf1bbcc8f5cde57d8b15c231404b
FerRueda <151107585+FerRueda@users.noreply.github.com>
2023-11-21 01:51:48 -0500

    Pasajero.java
    comentado

--- Avion2/Pasajero.java
+++ Avion2/Pasajero.java
@@ -1,39 +1,54 @@
-
-public class Pasajero
-{
-    //la fila y columna son atributos ya que en la matriz que representa el avion vamos a introducir el objeto Pasajero, no string. desde ese mismo objeto usamos getters para conseguir los datos necesarios
+public class Pasajero {
+    // Atributos de la clase Pasajero que representan la información del pasajero
+    // y su ubicación en el avión.
     private String nombre;
     private int precioTiquete;
     private int fila;
     private int columna;
-    //private int edad;
+    private int edad;
 
-    public Pasajero(String nombre, int precioTiquete, int fila, int columna)
-    {
-        this.nombre=nombre;
-        this.precioTiquete=precioTiquete;
-        this.columna = columna;
+    // Constructor de la clase Pasajero para inicializar sus atributos al crear un nuevo objeto Pasajero.
+    public Pasajero(String nombre, int precioTiquete, int edad, int fila, int columna) {
+        this.nombre = nombre;
+        this.precioTiquete = precioTiquete;
+        this.edad = edad;
         this.fila = fila;
-        
+        this.columna = columna;
     }
-    public String getNombre(){
+
+    // Método getter para obtener el nombre del pasajero.
+    public String getNombre() {
         return nombre;
     }
-    public String getprecioTiquete(){
-        return nombre;
+
+    // Método setter para establecer el nombre del pasajero.
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
     }
-    public void setNombre(String nombre){
-        this.nombre=nombre;
+
+    // Método getter para obtener el precio del tiquete del pasajero.
+    public int getPrecioTiquete() {
+        return precioTiquete;
     }
-     public void setPrecioTiquete(int precioTiquete){
-        this.precioTiquete=precioTiquete;
-    }   
-    public int getColumna(){
+
+    // Método setter para establecer el precio del tiquete del pasajero.
+    public void setPrecioTiquete(int precioTiquete) {
+        this.precioTiquete = precioTiquete;
+    }
+
+    // Método getter para obtener la columna en la que se encuentra el pasajero en el avión.
+    public int getColumna() {
         return columna;
     }
 
-    public int getFila(){
+    // Método getter para obtener la fila en la que se encuentra el pasajero en el avión.
+    public int getFila() {
         return fila;
     }
-    
+
+    // Método getter para obtener la edad del pasajero.
+    public int getEdad() {
+        return edad;
+    }
 }
+
