7542eddb9894ae035d1426f371940c5f33324a2e
FerRueda <151107585+FerRueda@users.noreply.github.com>
2023-11-21 02:05:47 -0500

    Avion.java
    comentado

--- Avion2/Avion.java
+++ Avion2/Avion.java
@@ -49,79 +49,132 @@ public class Avion {
             System.out.println("Archivo no existe: " + nombreArchivo);  // Imprimir un mensaje indicando que el archivo no existe
         }
     }
-
+    
     // Método para agregar un pasajero a una silla específica en el avión
     public boolean agregarPasajero(Pasajero pasajero) {
+        // Verificar si la fila proporcionada es válida
         if (pasajero.getFila() < 0 || pasajero.getFila() >= numFilas) {
             System.out.println("Fila es inválida");
+            // Devolver false para indicar que la operación de agregar pasajero ha fallado
             return false;
         }
+    
+        // Verificar si la columna proporcionada es válida para la fila dada
         if (pasajero.getColumna() < 0 || pasajero.getColumna() >= sillas[pasajero.getFila()].length) {
             System.out.println("Columna inválida");
+            // Devolver false para indicar que la operación de agregar pasajero ha fallado
             return false;
         }
+    
+        // Verificar si la silla seleccionada está disponible (no ocupada por otro pasajero)
         if (sillas[pasajero.getFila()][pasajero.getColumna()] == null) {
+            // Asignar la silla al pasajero
             sillas[pasajero.getFila()][pasajero.getColumna()] = pasajero;
+            // Imprimir un mensaje indicando que el pasajero ha sido asignado correctamente a la silla
             System.out.println("Pasajero " + pasajero.getNombre() + " asignado");
+            // Devolver true para indicar que la operación de agregar pasajero ha tenido éxito
             return true;
         } else {
+            // Imprimir un mensaje indicando que la silla está ocupada
             System.out.println("Silla ocupada");
+            // Devolver false para indicar que la operación de agregar pasajero ha fallado
             return false;
         }
     }
 
+
     // Método para calcular y devolver la cantidad total de dinero recaudado por la venta de boletos
     public int leerDinero() {
+        // Variable para acumular la cantidad total de dinero
         int acum = 0;
+    
+        // Iterar sobre las filas del avión
         for (int i = 0; i < sillas.length; i++) {
+            // Iterar sobre las columnas de cada fila
             for (int j = 0; j < sillas[i].length; j++) {
-                if (sillas[i][j] == null) continue;
-                else {
+                // Verificar si la silla está ocupada por un pasajero
+                if (sillas[i][j] == null) {
+                    // Si la silla está vacía, continuar con la siguiente iteración
+                    continue;
+                } else {
+                    // Si la silla está ocupada, acumular el precio del tiquete del pasajero actual
                     acum += sillas[i][j].getPrecioTiquete();
                 }
             }
         }
+    
+        // Devolver la cantidad total de dinero recaudado por la venta de boletos
         return acum;
     }
 
-    // Método para imprimir la estructura del avión, representando las sillas ocupadas con "+" y las vacías con "-"
+
+       // Método para imprimir la estructura del avión, representando las sillas ocupadas con "+" y las vacías con "-"
     public void imprimirEstructura() {
+        // Iterar sobre las filas del avión
         for (int i = 0; i < sillas.length; i++) {
+            // Iterar sobre las columnas de cada fila
             for (int j = 0; j < sillas[i].length; j++) {
-                if (sillas[i][j] == null) System.out.print("-");
-                else System.out.print("+");
+                // Verificar si la silla está ocupada por un pasajero
+                if (sillas[i][j] == null) {
+                    // Si la silla está vacía, imprimir "-" para representarla
+                    System.out.print("-");
+                } else {
+                    // Si la silla está ocupada, imprimir "+" para representarla
+                    System.out.print("+");
+                }
             }
+            // Imprimir una nueva línea después de imprimir todas las columnas de la fila actual
             System.out.println();
         }
     }
 
+
     // Método para imprimir los nombres de los pasajeros en el avión
     public void imprimirNombres() {
+        // Iterar sobre las filas del avión
         for (int i = 0; i < sillas.length; i++) {
+            // Iterar sobre las columnas de cada fila
             for (int j = 0; j < sillas[i].length; j++) {
-                if (sillas[i][j] == null) continue;
-                else System.out.println(sillas[i][j].getNombre());
+                // Verificar si la silla está ocupada por un pasajero
+                if (sillas[i][j] == null) {
+                    // Si la silla está vacía, continuar con la siguiente iteración
+                    continue;
+                } else {
+                    // Si la silla está ocupada, imprimir el nombre del pasajero
+                    System.out.println(sillas[i][j].getNombre());
+                }
             }
         }
     }
 
+
     // Método para calcular y devolver la edad promedio de los pasajeros en el avión
     public int calcularEdadPromedio() {
+        // Variables para contar la cantidad de pasajeros y acumular la suma de sus edades
         int cont = 0;
         int acum = 0;
-
+    
+        // Iterar sobre las filas del avión
         for (int i = 0; i < sillas.length; i++) {
+            // Iterar sobre las columnas de cada fila
             for (int j = 0; j < sillas[i].length; j++) {
-                if (sillas[i][j] == null) continue;
-                else {
+                // Verificar si la silla está ocupada por un pasajero
+                if (sillas[i][j] == null) {
+                    // Si la silla está vacía, continuar con la siguiente iteración
+                    continue;
+                } else {
+                    // Si la silla está ocupada, acumular la edad del pasajero y aumentar el contador
                     acum += sillas[i][j].getEdad();
                     cont++;
                 }
             }
         }
-
+    
+        // Calcular la edad promedio dividiendo la suma de edades por la cantidad de pasajeros
         int ans = acum / cont;
+        // Devolver la edad promedio calculada
         return ans;
     }
+
 }
 
